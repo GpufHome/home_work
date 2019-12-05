40e247bd4e502465a6372c9e3f10de5c33a59098
GpufHome <gpuf@bk.ru>
2019-12-05 08:41:52 +0800

    lesson4

--- MyApplication/app/src/main/java/com/example/myapplication/Main2Activity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,30 +1,85 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+    Toolbar toolbar;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        toolbarInit();
-
+        initView();
+        initSideMenu();
     }
 
-    private void toolbarInit() {
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
+    private void initSideMenu() {
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id  = menuItem.getItemId();
+        if (id ==  R.id.nav_f) {
+            imageView.setImageResource(R.drawable.f);
+        }
+
+        if (id ==  R.id.nav_o) {
+            imageView.setImageResource(R.drawable.o);
+        }
+        if (id ==  R.id.nav_p) {
+            imageView.setImageResource(R.drawable.p);
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    private void initView() {
+        imageView = findViewById(R.id.imageView);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        onBackPressed();
+
+        int id  = item.getItemId();
+        if (id ==  android.R.id.home) {
+            Log.d("myLogs", "Home");
+        }
+
+
+
+
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
